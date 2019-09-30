865212c2871a3252aea591423735dddf7c33c9de
Gincoeng <34204468+Gincoeng@users.noreply.github.com>
2019-09-30 23:44:53 +0800

    Add files via upload

---
+++ day1-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /**cin的使用方法*/
+
+    cout << "请输入密码：" << endl;
+    int password;
+    cin >> password;
+    cout << "你所输入的密码为：" << password << endl;
+    return 0;
+}
